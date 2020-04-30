1bb48c9c38b9eb6469e5cb196956d86889fa6d02
Andrew Morrow <acm@mongodb.com>
2020-04-30 09:09:09 -0400

    fixup

---
+++ testme.sh
@@ -0,0 +1,1 @@
+buildscripts/scons.py --variables-files=etc/scons/xcode_macosx.vars --link-model=dynamic --install-mode=hygienic --opt=off --dbg=on --build-fast-and-loose --implicit-cache --install-action=hardlink --cache ICECC= -j8 test-outcomes
