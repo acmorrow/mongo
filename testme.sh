932f2a6a2618db22fa808112f9d9eb59e9a81a30
Andrew Morrow <acm@mongodb.com>
2020-05-08 13:11:08 -0400

    better scripts

--- testme.sh
+++ testme.sh
@@ -1,1 +1,1 @@
-buildscripts/scons.py --variables-files=etc/scons/xcode_macosx.vars --link-model=dynamic --install-mode=hygienic --opt=off --dbg=on --build-fast-and-loose --implicit-cache --install-action=hardlink --cache ICECC= -j8 test-outcomes
+buildscripts/scons.py --variables-files=etc/scons/xcode_macosx.vars --link-model=dynamic --install-mode=hygienic --opt=off --dbg=on --build-fast-and-loose --implicit-cache --install-action=hardlink ICECC= CCACHE=ccache -j12 test-outcomes
