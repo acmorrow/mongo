0574bc989bfb0c0223f47e4062b188ded0186bc9
Andrew Morrow <acm@mongodb.com>
2020-05-08 13:13:01 -0400

    better scripts

--- testme.cache.sh
+++ testme.cache.sh
@@ -1,1 +1,11 @@
-buildscripts/scons.py --cache --variables-files=etc/scons/xcode_macosx.vars --link-model=dynamic --install-mode=hygienic --opt=off --dbg=on --build-fast-and-loose --implicit-cache --install-action=hardlink ICECC= CCACHE=ccache -j12 test-outcomes
+buildscripts/scons.py \
+  --cache \
+  --variables-files=etc/scons/xcode_macosx.vars \
+  --link-model=dynamic \
+  --install-mode=hygienic \
+  --opt=off --dbg=on 
+  --build-fast-and-loose --implicit-cache \
+  --install-action=hardlink \
+  ICECC= CCACHE=ccache \
+  -j12 \
+  test-outcomes
--- testme.sh
+++ testme.sh
@@ -1,1 +1,10 @@
-buildscripts/scons.py --variables-files=etc/scons/xcode_macosx.vars --link-model=dynamic --install-mode=hygienic --opt=off --dbg=on --build-fast-and-loose --implicit-cache --install-action=hardlink ICECC= CCACHE=ccache -j12 test-outcomes
+buildscripts/scons.py \
+  --variables-files=etc/scons/xcode_macosx.vars \
+  --link-model=dynamic \
+  --install-mode=hygienic \
+  --opt=off --dbg=on 
+  --build-fast-and-loose --implicit-cache \
+  --install-action=hardlink \
+  ICECC= CCACHE=ccache \
+  -j12 \
+  test-outcomes
